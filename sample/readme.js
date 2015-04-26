87477607c525c299a80936a299ba11d217408949
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-04-27 01:32:11 +0900

    add sample

---
+++ sample/readme.js
@@ -0,0 +1,18 @@
+require('../override');
+
+console.log([
+  '# Hello, *mdlog* World!',
+  '',
+  'you can use Markdown syntax on `console.log`.',
+  '',
+  '- Markdown is **powerfull**.',
+  '- Markdown is **useful**.',
+  '- Markdown is **readable**.',
+  '',
+  '> Markdown is a text-to-HTML conversion tool for web writers.',
+  'Markdown allows you to write using an easy-to-read, easy-to-write plain text format,',
+  'then convert it to structurally valid XHTML (or HTML).',
+  '',
+  'see <http://en.wikipedia.org/wiki/Markdown>.',
+].join('\n'));
+
