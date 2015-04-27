a07a7d6f48c7bf6a6e5b8907ed5ed640f10da10e
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-04-27 09:41:49 +0900

    add write method

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ config  = require('./config');
 var
 log = console.log;
 
-module.exports = function mdlog(markdown) {
+module.exports = function mdlog(markdown/*, ...args*/) {
   var
   string = convert(util.format.apply(null, arguments), config);
 
   log(string);
+};
+
+module.exports.write = function write(markdown/*, ...args*/) {
+  var
+  string = convert(util.format.apply(null, arguments), config);
+
+  process.stdout.write(string);
 };
