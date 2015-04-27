38ca969a8f78918bd87add0085ef24086554ee77
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-04-27 19:00:49 +0900

    fix override.js api

--- override.js
+++ override.js
@@ -1,1 +1,9 @@
-console.log = require('./index.js');
+var
+util  = require('util'),
+mdlog = require('./index.js');
+
+console._log = console.log;
+
+console.log = function mdlog_log(format/*, ...args*/) {
+  mdlog(util.format.apply(null, arguments));
+};
