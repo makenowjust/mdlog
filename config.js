b4dc461270a4d4e264a77761e71127107f4371a3
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-04-27 20:48:40 +0900

    use src directory

--- config.js
+++ config.js
@@ -1,24 +1,3 @@
-// loading node.js package
-var
-_ = require('lodash');
+'use strict';
 
-// loading default configuration
-var
-defaultConfig = require('./default_config');
-
-// parse LCSV (Labeled Comma Separated Values)
-function lcsv(string, def) {
-  return string.split(',').map(function (item) {
-    var
-    idx = item.indexOf(':');
-    if (idx === -1) {
-      return [item, true];
-    }
-    return [item.slice(0, idx), item.slice(idx + 1)];
-  }).reduce(function (lcsv, item) {
-    lcsv[item[0]] = item[1];
-    return lcsv;
-  }, def);
-}
-
-module.exports = lcsv(process.env.MDLOG_CONFIG || '', _.clone(defaultConfig));
+module.exports = require('./src/config');
