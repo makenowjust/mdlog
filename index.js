d5c918b8b749d397345811c98b6f45c92f0a34ac
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-05-03 10:28:27 +0900

    add comments

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 'use strict';
 
+// loading modules
 var
 prop    = require('./src/property'),
 convert = require('./src/convert')(prop),
 config  = require('./src/config');
 
+// make mdlog function
 var
 mdlog = require('./src/mdlog')(convert, config);
 
+// export other functions and `mdlog`
 mdlog.convert = convert;
 mdlog.config  = config;
 
--- override.js
+++ override.js
@@ -1,13 +1,17 @@
 'use strict';
 
+// loading packages
 var
 util  = require('util'),
 mdlog = require('./.');
 
+// backup `console.log`
 console._log = console.log;
 
+// override `console.log`
 console.log = function mdlog_log(format/*, ...args*/) {
   mdlog(util.format.apply(null, arguments));
 };
 
+// re-export `mdlog`
 module.exports = mdlog;
