0ca6639c5e2dfa13840d254c59cc9c2020549da4
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-06-03 22:51:37 +0900

    add document comments

--- es6/builder.js
+++ es6/builder.js
@@ -1,22 +1,36 @@
 'use strict';
 
+// import for `mdast.parse`.
 import mdast from 'mdast';
 
+// import configuration files.
 import mdastOpt     from '../config/mdast.json';
 import rule         from '../config/rule.json';
 import style        from '../config/style.json';
 import defaultColor from '../color/default.json';
 
 let
+// save original `console.log` for overriding it.
 log = console.log;
 
+// `mdlogBuilderBuilder` is a meta builder function for `mdlogBuilder`.
+//
+// An argument `Convert` is the subclass of `Convert` class
+// (e.g. `NodeConvert` or `BrowserConvert`.)
 export default function mdlogBuilderBuilder(Convert) {
+
+  // `mdlogBuilder` is the builder for `mdlog` function.
+  //
+  // An argument `color` is color scheme `Object` as dictionary.
   function mdlogBuilder(color) {
     return function mdlog(markdown) {
       log.apply(console, convert(markdown, color));
     };
   }
-
+ 
+  // `convert` converts Markdown into `console.log` styled text and arguments.
+  //
+  // It returns an `Array` for passing `console.log`.
   function convert(markdown, color) {
     if (typeof color === 'undefined') {
       color = defaultColor;
@@ -30,7 +44,8 @@ export default function mdlogBuilderBuilder(Convert) {
     return [text].concat(convert.args);
   }
 
+  // export `convert` in case.
   mdlogBuilder.convert = convert;
+
   return mdlogBuilder;
 };
-
